4042b75e6f581c8c8e4ff86b369ca8594ffb0097
RohithBoppey <rohithboppey1298@gmail.com>
2022-08-18 00:26:00 +0530

    Added Form Schema

--- back-end/src/admin/admin.js
+++ back-end/src/admin/admin.js
@@ -1,63 +1,47 @@
-const express = require('express')
-const NewAdmin = require('../Schema/AdminSchema')
+const express = require("express");
+const NewAdmin = require("../Schema/AdminSchema");
 
-const Router = express.Router()
+const Router = express.Router();
 
-Router.post('/happy',(req,res)=>{
-    NewAdmin.create(req.body)
-    console.log(req.body)
+Router.post("/NewAdmin", (req, res) => {
+	// try{
+	NewAdmin.create(req.body, function (err) {
+		if (err) return res.json(err);
+		res.send({
+			name: "karthik",
+		});
+	});
 
-    res.send({
-        name : "karthik"
-    })
-})
-
-Router.post('/NewAdmin',(req,res)=>{
-    // try{
-        NewAdmin.create(req.body,function(err) {
-            if (err) return res.json(err);
-            res.send({
-                name : "karthik"
-            })
-        })
-
-    // }
-    // catch(e){
-    //     res.send({
-    //         error:e
-    //     })
-    // }
-    
-    
-})
-Router.post('/',(req,res)=>{
-    // NewAdmin.create(req.body)
-    try {
-        NewAdmin.findOne({email:req.body.email},(e,user)=>{
-            if(user==null){
-                res.send({
-                    error:'error'
-                })
-                return ;
-            }
-            if(req.body.pass === user.password ){
-                
-                res.send({
-                    id:user.id
-                })
-                return;
-            }
-            res.send({
-                error:'error'
-            })
-  
-        })
-    } catch (error) {
-        
-    }
-
-    
-})
+	// }
+	// catch(e){
+	//     res.send({
+	//         error:e
+	//     })
+	// }
+});
 
 
-module.exports = Router
+Router.post("/", (req, res) => {
+	// NewAdmin.create(req.body)
+	try {
+		NewAdmin.findOne({ email: req.body.email }, (e, user) => {
+			if (user == null) {
+				res.send({
+					error: "error",
+				});
+				return;
+			}
+			if (req.body.pass === user.password) {
+				res.send({
+					id: user.id,
+				});
+				return;
+			}
+			res.send({
+				error: "error",
+			});
+		});
+	} catch (error) {}
+});
+
+module.exports = Router;
--- back-end/src/requests/requests.js
+++ back-end/src/requests/requests.js
@@ -2,3 +2,16 @@ const express = require('express');
 
 const Router = express.Router();
 
+const FormData = require("../Schema/FormSchema");
+
+Router.post("/newrequest", (req, res) => {
+    console.log(req.body);
+    FormData.create(req.body);
+    res.send(req.body)
+})
+
+Router.get("/newrequest", (req, res) => {
+    res.send({name : "Rohith"})
+})
+
+module.exports = Router;
